626bda82d2c1f02db7085ec90db29892a33b97cd
DAGMAX83 <DAGMAX983@gmail.com>
2019-01-06 19:21:33 -0600

    cleaning it up

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
-// Grab the articles as a json
+
 $.getJSON("/articles", function(data) {
-    // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
@@ -44,10 +43,8 @@ $.getJSON("/articles", function(data) {
   
   // When you click the savenote button
   $(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
   
-    // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
       method: "POST",
       url: "/articles/" + thisId,
